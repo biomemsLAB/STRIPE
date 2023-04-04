e02f9b0c84382b70967725950c899ad01bf1b52a
psteigerwald_kai <philipp.steigerwald@th-ab.de>
2023-04-04 16:56:51 +0200

    add FrameDataSet in utilities.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import numpy as np
 if __name__ == '__main__':
+    from utilities import FrameDataSet
+
+    frame = np.load("/mnt/MainNAS/temp/Persönliche Verzeichnisse/PS/save.npy", allow_pickle=True)
+    time_rows = frame[:, 0]
+    labels = frame[:, 1]
+    from torch.utils.data import Dataset, DataLoader
+    frame_dataset = FrameDataSet(time_rows, labels)
+    batch_size = 32
+    moving_mnist_dataloader = DataLoader(frame_dataset, batch_size=batch_size, shuffle=True)
     print("Welcome to STRIPE")
+
+
+

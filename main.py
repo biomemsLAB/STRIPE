d3dcab14ada8c82adc3d8c81c415f71dace23c2e
tivenide <tivenide@gmail.com>
2023-04-05 13:59:46 +0200

    create one big frame in main

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import numpy as np
 from utilities import WindowedFrameDataset
 if __name__ == '__main__':
     print("Welcome to STRIPE")
+
+    # create one big numpy array
+    path_to_dir_raw_data=''
+    path_to_save_numpy_array_raw=''
+    path_to_save_numpy_array_normalized=''
+
+    from utilities import preprocessing_for_multiple_recordings, save_frame_to_disk, load_frame_from_disk, normalize_frame
+    frame_of_multiple_recordings = preprocessing_for_multiple_recordings(path_to_dir_raw_data)
+    save_frame_to_disk(frame_of_multiple_recordings, path_to_save_numpy_array_raw)
+    #frame_of_multiple_recordings = load_frame_from_disk(path_to_save_numpy_array_raw)
+    frame_normalized = normalize_frame(frame_of_multiple_recordings, scaler_type='minmax')
+    save_frame_to_disk(frame_normalized, path_to_save_numpy_array_normalized)
+
+
     frame = np.load("/mnt/MainNAS/temp/Persönliche Verzeichnisse/PS/save.npy", allow_pickle=True)
     time_rows = frame[:, 0]
     labels = frame[:, 1]

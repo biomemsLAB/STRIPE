4d168675618a26542d9bc4f60625a4a56a473622
tivenide <tivenide@gmail.com>
2023-04-24 16:21:33 +0200

    commenting and adding loading_numpy_datasets_for_training() in main.py

--- main.py
+++ main.py
@@ -3,41 +3,21 @@ if __name__ == '__main__':
     print("Welcome to STRIPE")
 
     # init
-    from utilities import create_directory_structure, paths
-    path_to_working_dir = '/home/psteigerwald/PycharmProjects/STRIPE'
-    create_directory_structure(path_to_working_dir)
-    # getting paths
-    final_path_raw_train, final_path_raw_test, final_path_raw_val, final_path_raw_one, final_name_train_before_norm, final_name_test_before_norm, final_name_val_before_norm, final_name_one_before_norm = paths(path_to_working_dir)
+    from utilities import create_dataloader_simple, loading_numpy_datasets_for_training
+    path_to_working_dir = '/mnt/MainNAS/BioMemsLaborNAS/Projekt_Ordner/STRIPE'
 
-    # preprocessing
-    from utilities import preprocessing_for_multiple_recordings, save_frame_to_disk, load_frame_from_disk, normalize_frame
-    #frame_of_multiple_recordings = preprocessing_for_multiple_recordings(final_path_raw_one)
-    #save_frame_to_disk(frame_of_multiple_recordings, final_name_one_before_norm)
-    # normalization
-    #frame_of_multiple_recordings = load_frame_from_disk(final_name_one_before_norm)
-    scaler_type = 'minmax'
-    final_path_raw_train, final_path_raw_test, final_path_raw_val, final_path_raw_one, final_name_train_after_norm, final_name_test_after_norm, final_name_val_after_norm, final_name_one_after_norm = paths(path_to_working_dir, scaler_type=scaler_type)
-    #frame_normalized = normalize_frame(frame_of_multiple_recordings, scaler_type=scaler_type)
-    #save_frame_to_disk(frame_normalized, final_name_one_after_norm)
-    # final_name_one_after_norm = '/home/psteigerwald/PycharmProjects/STRIPE/data/save/before_normalization/one_frame_raw.npy'
-    #frame_of_multiple_recordings = load_frame_from_disk(final_name_one_before_norm)
-    # frame_of_multiple_recordings = load_frame_from_disk("/home/psteigerwald/PycharmProjects/STRIPE/data/save/one_frame_raw.npy")
-    frame_of_multiple_recordings = load_frame_from_disk("/home/psteigerwald/PycharmProjects/STRIPE/data/save/before_normalization/set_0001_new_sel_0000-0199_raw.npy")
-    # using this one frame for different dataloaders
-    from utilities import create_dataloader_simple
-    from utilities import dataset_pipeline_for_training_process
+    # loading preprocessed numpy arrays
+    data_train_balanced, label_train_balanced, data_test, label_test, data_val, label_val = loading_numpy_datasets_for_training(path_to_working_dir)
 
-    data_train_balanced, label_train_balanced, data_test, label_test, data_val, label_val = dataset_pipeline_for_training_process(frame_of_multiple_recordings)
+    print('train: spikes:', label_train_balanced.sum(), 'total:', len(label_train_balanced))
+    print('test: spikes:', label_test.sum(), 'total:', len(label_test))
+    print('val: spikes:', label_val.sum(), 'total:', len(label_val))
 
+    # creation of dataloader
     train_dataloader = create_dataloader_simple(data_train_balanced, label_train_balanced)
     test_dataloader = create_dataloader_simple(data_test, label_test)
     val_dataloader = create_dataloader_simple(data_val, label_val)
 
-    # using this frame just for one specific dataloader (train)
-    # frame = frame_of_multiple_recordings  #np.load("/mnt/MainNAS/temp/Persönliche Verzeichnisse/PS/save.npy", allow_pickle=True)
-    # from utilities import create_dataloader
-    # windowed_frame_dataloader = create_dataloader(frame)
-    # dataset_pipeline_for_training_process
     from nn_handle import handle_model
     from custom_models import DenseModel, ViT, CustomResNet, ResidualBlock, LSTM_Model
     dense_model = DenseModel(in_features=10, hidden_features=50, out_features=2)
@@ -45,7 +25,7 @@ if __name__ == '__main__':
     transformer_model = ViT(image_size=28, patch_size=7, num_classes=10, channels=1, dim=64, depth=6, heads=8, mlp_dim=128)
     lstm_model = LSTM_Model(in_features=10, hidden_features=128, out_features=2, num_layers=4)
     from nn_utilities import compare_models_acc_over_epoch
-    path_to_save = "/mnt/MainNAS/BioMemsLaborNAS/Projekt_Ordner/STRIPE/Results"
+    path_to_save = '' #"/mnt/MainNAS/BioMemsLaborNAS/Projekt_Ordner/STRIPE/Results"
     import torch
     torch.manual_seed(0)
     # Stuck Loss: https://datascience.stackexchange.com/questions/19578/why-my-training-and-validation-loss-is-not-changing

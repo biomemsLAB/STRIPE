5698e16118481944bf27b71c9737bdddce1b77ac
tivenide <tivenide@gmail.com>
2023-05-11 22:07:26 +0200

    adding embedding to TransformerModel() adding classes TransformerModel_without_embedding() and StackedTransformers()

--- custom_models_transformer.py
+++ custom_models_transformer.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
+from custom_models import DenseModel_based_on_FNN_SpikeDeeptector
+
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 200): # max_len equals maximum window size
         super(PositionalEncoding, self).__init__()
@@ -37,22 +39,91 @@ class TransformerModel(nn.Module):
         self.dropout = dropout
         self.device = device
 
+        self.embedding = nn.Linear(input_dim, hidden_size)
         self.pos_encoder = PositionalEncoding(hidden_size, dropout)
         encoder_layer = nn.TransformerEncoderLayer(hidden_size, num_heads, dim_feedforward=hidden_size*4, dropout=dropout, activation='gelu')
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers)
         self.fc = nn.Linear(hidden_size, num_classes)
-        #self.softmax = nn.Softmax(dim=1)
+        self.softmax = nn.Softmax(dim=1)
+        self.sigmoid = nn.Sigmoid()
+        self.selu = nn.SELU()
+
+        #self.dense = DenseModel_based_on_FNN_SpikeDeeptector(in_features=hidden_size, out_features=num_classes)
 
     def forward(self, src: Tensor) -> Tensor:
         batch_size, seq_len = src.shape[0], src.shape[1]
         src = src.view(batch_size, seq_len, self.input_dim)
         src = src.transpose(0, 1).contiguous()
+        src = self.embedding(src)
         src = self.pos_encoder(src)
         output = self.transformer_encoder(src)
         output = output.mean(dim=0)
+        #output = self.dense(output)
         output = self.fc(output)
         #output = self.softmax(output)
+        #output = self.sigmoid(output)
+        #pred = (output >= 0.9).float()
         return output
+
+
+class TransformerModel_without_embedding(nn.Module):
+    def __init__(self, input_dim: int, hidden_size: int, num_classes: int, num_layers: int, num_heads: int,
+                 dropout: float, device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
+        super(TransformerModel_without_embedding, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.device = device
+
+        #self.embedding = nn.Linear(input_dim, hidden_size)
+        self.pos_encoder = PositionalEncoding(hidden_size, dropout)
+        encoder_layer = nn.TransformerEncoderLayer(hidden_size, num_heads, dim_feedforward=hidden_size*4, dropout=dropout, activation='gelu')
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        self.softmax = nn.Softmax(dim=1)
+        self.sigmoid = nn.Sigmoid()
+        self.selu = nn.SELU()
+
+        #self.dense = DenseModel_based_on_FNN_SpikeDeeptector(in_features=hidden_size, out_features=num_classes)
+
+    def forward(self, src: Tensor) -> Tensor:
+        batch_size, seq_len = src.shape[0], src.shape[1]
+        src = src.view(batch_size, seq_len, self.input_dim)
+        src = src.transpose(0, 1).contiguous()
+        #src = self.embedding(src)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src)
+        output = output.mean(dim=0)
+        #output = self.dense(output)
+        output = self.fc(output)
+        #output = self.softmax(output)
+        #output = self.sigmoid(output)
+        #pred = (output >= 0.9).float()
+        return output
+
+
+class StackedTransformers(nn.Module):
+    def __init__(self, device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
+        super(StackedTransformers, self).__init__()
+
+        self.device = device
+        self.model1 = TransformerModel(input_dim=1, hidden_size=256, num_classes=2, num_layers=6, num_heads=8,
+                                       dropout=0.1)
+        self.model2 = TransformerModel_without_embedding(input_dim=1, hidden_size=256, num_classes=2, num_layers=6,
+                                                         num_heads=8, dropout=0.1)
+        self.fc = nn.Linear(4, 2)
+        self.softmax = nn.Softmax(dim=1)
+    def forward(self, x):
+        x1 = self.model1(x)
+        x2 = self.model2(x)
+        out_cat = torch.cat([x1, x2], dim=1)
+        out_cat = self.fc(out_cat)
+        out_cat = self.softmax(out_cat)
+        return out_cat
 
 
 ## following architectures are not working properly
